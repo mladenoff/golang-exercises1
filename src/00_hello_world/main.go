91df640d9676c9d631f40c13b78e49441b2cedd7
Isak Mladenoff <imladenoff@gmail.com>
2018-04-18 19:34:18 -0700

    Complete hello world

--- src/00_hello_world/main.go
+++ src/00_hello_world/main.go
@@ -1,3 +1,18 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
 //main
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter your name, friend.")
+	text, _ := reader.ReadString('\n')
+	text = strings.Trim(text, "\n")
+	fmt.Printf("Hello %v!\n", text)
+}
